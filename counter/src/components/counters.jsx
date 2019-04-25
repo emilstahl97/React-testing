3076d663a351e307a9fb0e53f453223c587553f2
Emil Ståhl <emil.stahl@icloud.com>
2019-04-25 16:49:12 +0200

    do not remember

--- counter/src/components/counters.jsx
+++ counter/src/components/counters.jsx
@@ -11,19 +11,34 @@ class Counters extends Component {
     ]
   };
 
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
   handleDelete = counterId => {
-    console.log("Event Handler Called", counterId);
+    const counters = this.state.counters.filter(c => c.id != counterId);
+    this.setState({ counters });
   };
 
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          {" "}
+          Reset{" "}
+        </button>
         {this.state.counters.map(counter => (
           <Counter
             key={counter.id}
             onDelete={this.handleDelete}
-            value={counter.value}
-            id={counter.id}
+            counter={counter}
           />
         ))}
       </div>
